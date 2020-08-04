3be0d142803f0133a707674797dc64ade0f3b05f
Sarah Drasner <sarah.drasner@gmail.com>
2020-08-04 15:53:12 -0600

    add in the game resources

---
+++ gameresources/questions.js
@@ -0,0 +1,38 @@
+const questions = [
+  {
+    question: `What's your dog's name?`,
+    baker: "Woofgang Puck",
+    mechanic: "Alf",
+    artist: "Salvador Dogi",
+  },
+  {
+    question: `What's your favorite hobby?`,
+    baker: "Extreme ironing",
+    mechanic: "Bacon santa cosplay",
+    artist: "Mimosas",
+  },
+  {
+    question: `What's your favorite color?`,
+    baker: "turquoise",
+    mechanic: "yellow",
+    artist: "transparent",
+  },
+  {
+    question: `Is cereal soup?`,
+    baker: "You can't be serieal",
+    mechanic: "Yes, I am Jason Lengstorf",
+    artist: "wut",
+  },
+  {
+    question: `What’s the most imaginative insult you can come up with?`,
+    baker: "You're a substitute teacher with no lesson plan",
+    mechanic: "Yer face is a melted welly",
+    artist: "You eat buttons off the remote",
+  },
+  {
+    question: `If peanut butter wasn’t called peanut butter, what would it be called?`,
+    baker: "legoome",
+    mechanic: "brown paste",
+    artist: "groundnut smoosh",
+  },
+],
