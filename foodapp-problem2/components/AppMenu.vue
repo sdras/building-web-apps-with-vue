5b12ad5beefa1bfb01ef3de251d127a759f6c748
Sarah Drasner <sarah.drasner@gmail.com>
2020-08-15 17:46:44 -0600

    fix up problem and solution 2

--- foodapp-problem2/components/AppMenu.vue
+++ foodapp-problem2/components/AppMenu.vue
@@ -13,15 +13,11 @@
 
 <script>
 import { mapGetters } from "vuex";
-import AppLogo from "@/components/AppLogo.vue";
 
 export default {
-  components: {
-    AppLogo
-  },
   computed: {
-    ...mapGetters(["cartCount"])
-  }
+    ...mapGetters(["cartCount"]),
+  },
 };
 </script>
 
