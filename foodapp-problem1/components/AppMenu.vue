4efb1a51242ffef91017843f8ce7a0f1efd7cd92
Sarah Drasner <sarah.drasner@gmail.com>
2020-08-15 16:26:59 -0600

    add in first problem

---
+++ foodapp-problem1/components/AppMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <nuxt-link exact to="/">Home</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import AppLogo from "@/components/AppLogo.vue";
+
+export default {
+  components: {
+    AppLogo
+  },
+  computed: {
+    ...mapGetters(["cartCount"])
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ foodapp-problem1/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <AppHeader />
+    <AppRestaurantInfo />
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
+
+export default {
+  setup() {
+    console.log("Foobar");
+  },
+  components: {
+    AppHeader,
+    AppRestaurantInfo: () =>
+      import(/* webpackPrefetch: true */ "@/components/AppRestaurantInfo.vue")
+  },
+  computed: {
+    fooddata() {
+      return this.$store.state.fooddata;
+    }
+  }
+};
+</script>
+
+<style></style>
