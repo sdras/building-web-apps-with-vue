f59c52c9ad8af0d93d95ec4e372d126646026cad
Sarah Drasner <sarah.drasner@gmail.com>
2020-08-15 16:22:36 -0600

    add in solution1

---
+++ foodapp-solution1/components/AppMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <nuxt-link exact to="/">Home</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/restaurants">Restaurants</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import AppLogo from "@/components/AppLogo.vue";
+
+export default {
+  components: {
+    AppLogo
+  },
+  computed: {
+    ...mapGetters(["cartCount"])
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
