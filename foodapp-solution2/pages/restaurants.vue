d089c5c9c1e0d96af05e8690f15bd120ec0dc39a
Sarah Drasner <sarah.drasner@gmail.com>
2020-08-15 16:10:20 -0600

    add in second solution

---
+++ foodapp-solution2/pages/restaurants.vue
@@ -0,0 +1,37 @@
+<template>
+  <main class="container">
+    <div class="restaurantheading">
+      <h1>Restaurants</h1>
+    </div>
+
+    <AppRestaurantInfo />
+  </main>
+</template>
+
+<script>
+import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
+
+export default {
+  components: {
+    AppSelect,
+    AppRestaurantInfo: () =>
+      import(/* webpackPrefetch: true */ "@/components/AppRestaurantInfo.vue")
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.restaurantheading {
+  margin: 100px 0 20px 200px;
+  display: flex;
+  align-items: center;
+}
+
+h1 {
+  margin-right: 40px;
+}
+
+.filter {
+  padding-right: 200px;
+}
+</style>
