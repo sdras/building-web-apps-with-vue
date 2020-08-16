7b1e1c2a16d9801f2079f9c2d661ffe1050ce772
Sarah Drasner <sarah.drasner@gmail.com>
2020-08-15 19:16:17 -0600

    cleanup

--- foodapp-problem3/pages/index.vue
+++ foodapp-problem3/pages/index.vue
@@ -10,19 +10,16 @@ import AppHeader from "@/components/AppHeader.vue";
 import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
 
 export default {
-  setup() {
-    console.log("Foobar");
-  },
   components: {
     AppHeader,
     AppRestaurantInfo: () =>
-      import(/* webpackPrefetch: true */ "@/components/AppRestaurantInfo.vue")
+      import(/* webpackPrefetch: true */ "@/components/AppRestaurantInfo.vue"),
   },
   computed: {
     fooddata() {
       return this.$store.state.fooddata;
-    }
-  }
+    },
+  },
 };
 </script>
 
