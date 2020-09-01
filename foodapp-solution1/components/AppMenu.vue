fdeb575370e34c936f6d04ce616024b700659010
Sarah Drasner <sarah.drasner@gmail.com>
2020-09-01 13:53:20 -0600

    add in solution 1

---
+++ foodapp-solution1/components/AppMenu.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <nuxt-link exact to="/">Home</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/restaurants">Restaurants</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped></style>
---
+++ foodapp-solution1/pages/restaurants.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="container restaurant">
+    <div class="restaurantheading">
+      <h1>Restaurants</h1>
+    </div>
+  </main>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+</style>
