8f40eb0a13061269d3b0bcda5787af547fcae9df
Sarah Drasner <sarah.drasner@gmail.com>
2020-09-01 13:37:30 -0600

    kick off solution 2 again

---
+++ foodapp-solution2/components/AppMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <nuxt-link exact to="/">
+          <AppLogo />
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/restaurants">Restaurants</nuxt-link>
+      </li>
+    </ul>
+    <nuxt-link to="/cart">Cart</nuxt-link>
+  </nav>
+</template>
+
+<script>
+import AppLogo from "@/components/AppLogo.vue";
+
+export default {
+  components: {
+    AppLogo,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ foodapp-solution2/components/AppRestaurantInfo.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="restaurantinfo">
+    <div v-for="store in datasource" :key="store.id">
+      <h2>{{ store.name }}</h2>
+      <p>Delivery Time {{ store.deliveryTime }}</p>
+      <p>Rating {{ store.rating }}</p>
+      <p v-if="store.freeDelivery" class="label">
+        <span>Free Delivery</span>
+      </p>
+      <div class="row">
+        <div
+          v-for="menuitem in store.menu"
+          :key="menuitem.id"
+          :style="`background: url(/${menuitem.img}) no-repeat center center`"
+          class="fooditems"
+        >
+          <div class="iteminfo">
+            <div class="menuiteminfo">
+              <h4>{{ menuitem.item }}</h4>
+              <p>{{ prices(menuitem) }}</p>
+            </div>
+            <nuxt-link :to="`item/${menuitem.id}`">
+              <button class="ghost">View Item ></button>
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    datasource: {
+      type: [Array, Object],
+    },
+  },
+  methods: {
+    prices(menuitem) {
+      return "$" + menuitem.price.toFixed(2);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
