f9e262cc4d0c41dd458973e75ab19840dd86e5b2
Sarah Drasner <sarah.drasner@gmail.com>
2020-09-01 10:52:20 -0600

    update footer

--- foodapp-solution3/components/AppFooter.vue
+++ foodapp-solution3/components/AppFooter.vue
@@ -5,17 +5,4 @@
 </template>
 
 <style lang="scss" scoped>
-footer {
-  background: black;
-  width: 100%;
-  padding: 10px 30px;
-  color: white;
-  text-align: center;
-}
-a,
-a:visited,
-a:active {
-  color: #c4c3ec;
-  font-weight: bold;
-}
 </style>
